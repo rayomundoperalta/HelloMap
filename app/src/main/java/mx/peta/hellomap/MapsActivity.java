04f572d19531585dde93c7c0942ec5007d9eb631
Raymundo Peralta <movile@peta.mx>
2016-10-31 21:17:34 -0600

    funciona coordenasa, mapa y se trae foto del directorio de la camara, no esta habilitada la toma de fotografia

--- app/src/main/java/mx/peta/hellomap/MapsActivity.java
+++ app/src/main/java/mx/peta/hellomap/MapsActivity.java
@@ -2,11 +2,22 @@ package mx.peta.hellomap;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
+import android.text.format.DateFormat;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,13 +26,35 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
 import mx.peta.hellomap.servicios.ServicioGPS;
+
+import static android.R.attr.bitmap;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ImageView mImageView;
     private Bitmap mImageBitmap;
+    private Button btnFin;
+
+    File photoFile = null;
+
+    int count = 0;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +65,69 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         TextView t = (TextView) findViewById(R.id.textoUbicacion);
         servicioGPS.setView(t);
 
-        mImageView = (ImageView) findViewById(R.id.imageView1);
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, 100);
-        }
+        btnFin = (Button) findViewById(R.id.btnFin);
+        btnFin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mImageView = (ImageView) findViewById(R.id.imageView1);
+        mImageView.setClickable(true);
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Image Touch", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //mImageView.setImageURI(Uri.fromFile(testCreateImageFile()));
+        String path = testCreateImageFile().getAbsolutePath();
+        Bitmap bitmap=null;
+        //File f= new File(path);
+        //BitmapFactory.Options options = new BitmapFactory.Options();
+        //options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+        //try {
+        //    bitmap = BitmapFactory.decodeStream(new FileInputStream(f), null, options);
+        //} catch (FileNotFoundException e) {
+        //    e.printStackTrace();
+        //    Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG).show();
+        //}
+        //mImageView.setImageBitmap(bitmap);
+        bitmap = BitmapFactory.decodeFile(path);
+        int bAlto = bitmap.getHeight();
+        int bAncho = bitmap.getWidth();
+
+        float scale = Math.min((float) 150.0 / (float) bAlto, (float) 150.0 / (float) bAncho);
+        Bitmap chico = Bitmap.createScaledBitmap(bitmap,(int)(bAncho*scale), (int)(bAlto*scale), true);
+        mImageView.setImageBitmap(chico);
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Se verifica si hay una aplicacion que pueda tomar la foto
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // se crea el archivo donde se guardara la foto
+            photoFile = createImageFile();
+
+            if (photoFile != null) {
+                Uri photoURI = Uri.fromFile(photoFile);
+                //Toast.makeText(getApplicationContext(),  photoURI.toString(),Toast.LENGTH_LONG).show();
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            }
+            ++count;
+            Toast.makeText(getApplicationContext(), "Count " + count, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Count " + count, Toast.LENGTH_LONG).show();
+            // startActivityForResult(takePictureIntent, 100);
+        }
     }
 
 
@@ -65,12 +151,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mexico, 13));
     }
 
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 100 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
+            mImageView.setImageURI(Uri.fromFile(photoFile));
         }
+    }
+
+    private File createImageFile()  {
+        // Create an image file name
+        // the image will be stored in the DCIM directory
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTimeZone(TimeZone.getDefault());
+        long dateTaken = calendar.getTimeInMillis();
+
+        File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() + "/Camera/");
+        if (!dir.exists()) dir.mkdirs();
+        final File photoFile = new File(dir, DateFormat.format("yyyyMMdd_kkmmss", dateTaken).toString() + ".jpg");
+
+        return photoFile;
+    }
+
+    private File testCreateImageFile() {
+        File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() + "/Camera/");
+        if (!dir.exists()) dir.mkdirs();
+        final File photoFile = new File("/storage/emulated/0/DCIM/Camera/", "20161031_151205.jpg");
+
+        return photoFile;
     }
 }
